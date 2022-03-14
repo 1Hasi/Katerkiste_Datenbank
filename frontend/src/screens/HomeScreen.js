aacfbc58619234ac0ba7e4b69f907119738cd09f
dennis1900 <dennis.prichodko59@gmail.com>
2022-03-14 22:54:10 +0100

    css änderung 2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,33 +6,38 @@ import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import DOMPurify from "dompurify";
 
+
+
 export default function HomeScreen(props) {
 return (																				
 <div>
-<div classname="cover" > 																
+<div className="cover" > 																
     <img src="img/beer2.png" alt="beercover"></img>
     <span>Kater war gestern.<br></br>
          Heut' schon was getrunken? <br></br>
     </span>
 
-    <a href="Angebote.html">
-        <button type="button" name="cover_button">Durst löschen
-        </button>
-    </a>
+    <div> 
+        <Link to="/angebote">
+            <button type="button" name="cover_button">Durst löschen
+            </button>
+        </Link>
+    </div>
+    
 
-    <img classname="cover_icon" src="img/katerkistenlogogold.png"
+    <img className="cover_icon" src="img/katerkistenlogogold.png"
                 alt="katerkistenlogogold" ></img>
 </div>																								
 
-<div classname="content">															
+<div className="content">															
 
-    <div classname="row left">
-        <div classname="horizontal_line"></div>
+    <div className="row left">
+        <div className="horizontal_line"></div>
     </div>
 
-    <div classname="row left">													
-        <div classname="h_content col-6 left">
-                <a href="Angebote.html"><h2>Tipp des Monats</h2></a>
+    <div className="row left">													
+        <div className="h_content col-6 left">
+                <Link to="/angebote"><h2>Tipp des Monats</h2></Link>
                     <p>Ihr habt abgestimmt!
                         <br></br>
                             Hier ist er, euer Tipp des Monats.
@@ -42,30 +47,30 @@ return (
                         mit Freunden und Familie.
                     </p>
 
-                <div classname="zu_ang">
-                     <a href="Angebote.html">
+                <div className="zu_ang">
+                     <Link to="/angebote">
                          <button type="button" name="zum_angebot">Zum Angebot
                          </button>
-                     </a>
+                     </Link>
                  </div>
             </div>
 
-        <div classname="h_content col-6 left tdm">
-            <a href="Angebote.html">
+        <div className="h_content col-6 left tdm">
+            <Link to="/angebote">
                 <img src="img/angebote/tipp_des_monats.png" alt="Tipp des Monats"></img>
-            </a>
+            </Link>
         </div>
     </div>
 
-    <div classname="row left">
-        <div classname="horizontal_line"></div>
+    <div className="row left">
+        <div className="horizontal_line"></div>
     </div>
 
-    <div classname="row left">												
-        <div classname="h_content col-6 left laster">
+    <div className="row left">												
+        <div className="h_content col-6 left laster">
                 <img src="img/lastwagen_kater.png" alt="Express Lieferung"></img>
         </div>
-        <div classname="h_content col-6 left">
+        <div className="h_content col-6 left">
                 <h2>Express Lieferung</h2>
                 <p>Wir behandeln eure Aufträge so fix wie kein anderer.
                     <br></br>
@@ -82,12 +87,12 @@ return (
         </div>
     </div>
 
-    <div classname="row left">
-        <div classname="horizontal_line"></div>
+    <div className="row left">
+        <div className="horizontal_line"></div>
     </div>
 
-    <div classname="row left">																
-        <div classname="h_content col-6 left">
+    <div className="row left">																
+        <div className="h_content col-6 left">
                 <a href="Angebote.html"><h2>Angebote</h2></a>
                 <p>Wir bieten die unterschiedlichsten Sets für jede Situation an.
                     <br></br>
@@ -98,18 +103,18 @@ return (
                     Sicherheit auch was für euch dabei!
                  </p>
 
-                 <div classname="zu_ang">
-                   <a href="Angebote.html">
+                 <div className="zu_ang">
+                   <Link to="/angebote">
                           <button type="button" name="zum_angebot">Zum Angebot
                           </button>
-                      </a>
+                      </Link>
                   </div>
 
             </div>
-        <div classname="h_content col-6 left getraenke">
-                <a href="Angebote.html">
+        <div className="h_content col-6 left getraenke">
+                <Link to="/angebote">
                     <img src="img/Getränke.png" alt="Getränke"></img>
-                </a>
+                </Link>
         </div>
     </div>
 </div>																							
